b8a44daf98663c94ceb13b70287ed5c3fa46320f
Nicolas Julémont <nicolas.julemont@gmail.com>
2022-03-20 14:27:20 +0100

    Initial map.

--- make_map.py
+++ make_map.py
@@ -8,39 +8,30 @@ def main():
     #
     m = folium.Map(location=[50.800000, 4.416667], zoom_start=15)
 
-    data = pd.read_csv('survey.csv')
-    data['latitude'] = data['LatLng'].map(lambda x: str(x).split(';')[0])
-    data['longitude'] = data['LatLng'].map(lambda x: str(x).split(';')[1])
-
-    data['latitude_g'] = data['gracq_lat_lng'].map(
-        lambda x: str(x).split(';')[0] if len(str(x).split(';')) == 2 else -1)
-    data['longitude_g'] = data['gracq_lat_lng'].map(
-        lambda x: str(x).split(';')[1] if len(str(x).split(';')) == 2 else -1)
-
-    data['text'] = 'Remarque : ' + data['remark'] + '\n' + 'Nombre arceaux : ' + data['n_arceaux'].map(lambda x : str(x))
-    data['id'] = data['id']
+    data = pd.read_csv('data.csv')
+    data['latitude'] = data['lat_lng'].map(lambda x: str(x).split(';')[0])
+    data['longitude'] = data['lat_lng'].map(lambda x: str(x).split(';')[1])
 
     for row in data.itertuples():
         lat = row.latitude
         lng = row.longitude
-        text = row.text
+        status = row.status_commune
+        n_arceaux = row.n_arceaux_form
+        comment = row.comment_commune
         id = row.id
-        done = row.done
-        lat_G = row.latitude_g
-        lng_G = row.longitude_g
 
-        # if 'V' in done:
-        #     Marker(icon=Icon(color="green"), location=[lat, lng], popup=f'<b>id {id}</b>').add_to(m)
-        # else:
-        #     Marker(icon=Icon(color="red"), location=[lat, lng], popup=f'<b>id {id}</b>{text}').add_to(m)
-
-        if lng_G != -1:
-            Marker(icon=Icon(color="blue"), location=[lat_G, lng_G], popup=f'<b>id {id}</b>').add_to(m)
-
+        if lng != -1:
+            Marker(icon=Icon(color="blue"),
+                   location=[lat, lng],
+                   popup=f'<b>Id : {id} </b> [{status}]<br />'
+                         f'<hr>'
+                         f'Commentaire commune : {comment}<br />'
+                         f'<hr>'
+                         f'Nombre arceaux demandés : {n_arceaux}'
+                   ).add_to(m)
 
     # Display the map
     m.save("map.html")
-    webbrowser.open("map.html")
 
 
 # Press the green button in the gutter to run the script.
